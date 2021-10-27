cda441389cbce29808000fce179d795fc5d99b0d
Giadaspt <saputo.giada@gmail.com>
2021-10-27 18:51:02 +0200

    foto cambiano da html

--- js/script.js
+++ js/script.js
@@ -1,61 +1,51 @@
 
-const right_slide = [
-  'img/01.jpg',
-  'img/02.jpg',
-  'img/03.jpg',
-  'img/04.jpg',
-  'img/05.jpg'
-];
-
-//const box_slide = document.getElementsByClassName('box-slide');
-//const right_slide = document.getElementsByClassName('right-slide-inner');
-//console.log(box_slide);
-//console.log(right_slide);
-let box_slide = [...right_slide];
-console.log(box_slide);
-
-let wrappper = document.querySelector('.wrapper');
+const box_slide = document.getElementsByClassName('box-slide');
+const right_slide = document.getElementsByClassName('right-slide-inner');
+const slideAll = [...new Set(box_slide,right_slide) ];
+console.log(slideAll);
 
 let counter = 0;
-//console.log(counter);
-//console.log(box_slide [counter]);
-//console.log(right_slide [counter]);
+//console.log(box_slide[counter]);
+//console.log(right_slide[counter]);
+
 
 const up_chevron = document.querySelector('.up-chevron');
 const down_chevron = document.querySelector('.down-chevron');
-//console.log(up_chevron);
-//console.log(down_chevron);
 
 up_chevron.addEventListener('click', function(){
+  slideAll[counter].classList.remove('active');
   counter++;
   if (counter > 4) {
     counter = 0;
   }
-  console.log(counter);
+  slideAll[counter].classList.add('active');
+  console.log(slideAll[counter]);
 });
 
 down_chevron.addEventListener('click', function(){
+  slideAll[counter].classList.remove('active');
   counter--;
   if (counter < 0) {
-    counter = right_slide.length -1;
+    counter = slideAll.length -1;
   }
-  console.log(counter);
+  slideAll[counter].classList.add('active');
+  console.log(slideAll[counter]);
 });
 
 
-for (i = 0; i < box_slide.length; i++) {
-  console.log(box_slide[i]);
+// for (i = 0; i < box_slide.length; i++) {
+//   console.log(box_slide[i]);
 
-  const element = document.createElement('div');
-  element.className = 'element';
+//   const element = document.createElement('div');
+//   element.className = 'element';
 
-  if (i == counter) {
-    element.classList.add('active');
-  }
+//   if (i == counter) {
+//     element.classList.add('active');
+//   }
  
-  element.innerHTML = `
-  <img src=${box_slide[i]}" alt="">
-  `;
+//   element.innerHTML = `
+//   <img src=${box_slide[i]}" alt=">
+//   `;
   
-  wrappper.append(element);
-}
+//   wrappper.append(element);
+// }
