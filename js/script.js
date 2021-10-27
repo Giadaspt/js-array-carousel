14e10d91e758142fe2ca3209cd6beaabe8370f1d
Giadaspt <saputo.giada@gmail.com>
2021-10-27 22:29:44 +0200

    inserite foto array

--- js/script.js
+++ js/script.js
@@ -1,13 +1,53 @@
+const slideAll = [
+  'img/01.jpg',
+  'img/02.jpg',
+  'img/03.jpg',
+  'img/04.jpg',
+  'img/05.jpg'
+];
+// console.log(slideAll);
 
-const box_slide = document.getElementsByClassName('box-slide');
-const right_slide = document.getElementsByClassName('right-slide-inner');
-const slideAll = [...new Set(box_slide,right_slide) ];
-console.log(slideAll);
+const slider = document.querySelector('.slider');
+const sliderBig = document.querySelector('.out-box-slide');
+const sliderR = document.querySelector('.right-slide');
+
+
+// const box_slide = document.getElementsByClassName('box-slide');
+// const right_slide = document.getElementsByClassName('right-slide-inner');
+// const slideAll = [...new Set(box_slide,right_slide) ];
+
+// const slideAll = [...box_slide, ...right_slide];
 
 let counter = 0;
 //console.log(box_slide[counter]);
 //console.log(right_slide[counter]);
 
+for (i = 0; i < slideAll.length ; i++) {
+
+  const item1 = document.createElement('div');
+  const item2 = document.createElement('div');
+  //console.log(item1);
+  //console.log(item2);
+  item1.className = 'item1';
+  item2.className = 'item2';
+
+  if (i === 0){
+    item1.classList.add('active');
+    item2.classList.add('active');
+  } else  {
+    item1.classList.remove('active');
+    item2.classList.remove('active');
+  }
+
+  
+  sliderBig.append(item1);
+  sliderR.append(item2);
+  console.log(sliderBig);
+  console.log(sliderR);
+  
+  item1.innerHTML = `<img src="${slideAll[i]}" alt="">`;
+  item2.innerHTML = `<img src="${slideAll[i]}" alt="">`;
+}
 
 const up_chevron = document.querySelector('.up-chevron');
 const down_chevron = document.querySelector('.down-chevron');
@@ -31,21 +71,3 @@ down_chevron.addEventListener('click', function(){
   slideAll[counter].classList.add('active');
   console.log(slideAll[counter]);
 });
-
-
-// for (i = 0; i < box_slide.length; i++) {
-//   console.log(box_slide[i]);
-
-//   const element = document.createElement('div');
-//   element.className = 'element';
-
-//   if (i == counter) {
-//     element.classList.add('active');
-//   }
- 
-//   element.innerHTML = `
-//   <img src=${box_slide[i]}" alt=">
-//   `;
-  
-//   wrappper.append(element);
-// }
